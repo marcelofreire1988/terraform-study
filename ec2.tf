d557d6f5e06fac92b49fc260556150c62b4915bd
marcelo <marcelo.freire88@gmail.com>
2020-04-12 11:26:37 -0300

    ajustes

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,10 @@
-
 resource "aws_instance" "web" {
-  count = var.servers
+  count = var.count-servers
   ami = var.amis.us-east-1
-  instance_type = "t2.micro"
-  vpc_security_group_ids =  ["${aws_security_group.acesso-ssh.id}"]
-  key_name      = var.key_name
-
+  instance_type = var.machine-size.regular-size
+  vpc_security_group_ids = ["${aws_security_group.enable-ssh-access.id}"]
+  key_name = var.key-name
+  tags = {
+    Name = "study-machine-${count.index}"
+  }
 }
-
-//resource "aws_instance" "ubuntu-marcelo" {
-//  provider = "aws.us-east-2"
-//  ami = var.amis.us-east-2
-//  instance_type = "t2.micro"
-//  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
-//}
--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
 output "ip_address" {
-    # referencia para o valor criado pelo aws_instance(web) e apresenta o ip
-    value = "${aws_instance.web[*].public_ip}"
+  value = "${aws_instance.web[*].public_ip}"
 }
 
