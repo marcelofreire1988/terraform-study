d7c495fc53265c1c5e8edd6735b905838f29d591
marcelofreire88 <marcelo.freire88@gmail.com>
2020-04-16 11:41:15 -0300

    uso do terraform fmt utilizando a versão 0.12

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  count = var.count-servers
-  ami = var.amis.us-east-1
-  instance_type = var.machine-size.regular-size
-  vpc_security_group_ids = ["${aws_security_group.enable-ssh-access.id}"]
-  key_name = var.key-name
+  count                  = var.count-servers
+  ami                    = var.amis.us-east-1
+  instance_type          = var.machine-size.regular-size
+  vpc_security_group_ids = [aws_security_group.enable-ssh-access.id]
+  key_name               = var.key-name
   tags = {
     Name = "study-machine-${count.index}"
   }
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "ip_address" {
-  value = "${aws_instance.web[*].public_ip}"
+  value = aws_instance.web[*].public_ip
 }
 
