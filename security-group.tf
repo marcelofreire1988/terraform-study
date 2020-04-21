a907644b919e9c9c2d1b67b89e7654ee26efb5e8
00793390427 <00793390427>
2020-04-21 12:04:38 -0300

    ajuste no security-group

--- security-group.tf
+++ security-group.tf
@@ -14,11 +14,13 @@ resource "aws_security_group" "enable-ssh-access" {
     }
   }
   egress {
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = [
+    "0.0.0.0/0"]
   }
   tags = {
     Name = "Egress"
   }
+}
